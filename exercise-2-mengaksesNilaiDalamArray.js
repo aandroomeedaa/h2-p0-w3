5d55e92cf0c30793e857cdc1a84e4a8df229dbd0
aandroomeedaa <41657603+aandroomeedaa@users.noreply.github.com>
2018-08-29 15:18:37 +0700

    Update exercise-2-mengaksesNilaiDalamArray.js

--- exercise-2-mengaksesNilaiDalamArray.js
+++ exercise-2-mengaksesNilaiDalamArray.js
@@ -1,7 +1,14 @@
-var st = 'hello World'
+function balikKata(st){
 var tampung = ''
 for (var i= st.length-1 ; i>=0 ; i--)
 {
   tampung = tampung + st[i] 
 } 
-console.log (tampung)
+return (tampung)
+}
+//TESTCASE
+console.log(balikKata('apakabar'))
+console.log(balikKata('ISI APA AJA DAH BEBAS TERSERAH LU'))
+console.log(balikKata('INVOKER GOT TRIPLE KILL'))
+console.log(balikKata('hello world'))
+console.log(balikKata('VOID GOT SEVEN DIVINE RAPIER'))
