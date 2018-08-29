0f8c09819a312df697d7cd6db394dcc1824b6f23
aandroomeedaa <41657603+aandroomeedaa@users.noreply.github.com>
2018-08-29 15:24:03 +0700

    Update exercise3-melakukanLoopingDataArray.js

--- exercise3-melakukanLoopingDataArray.js
+++ exercise3-melakukanLoopingDataArray.js
@@ -1,9 +1,4 @@
-var input = [
-                ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
-                ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
-                ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
-                ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
-            ]
+function datHAN(input){
 
 for (var i=0 ;i<input.length;i++){
   console.log(' ')
@@ -16,8 +11,15 @@ else if(j===2)
 console.log('TTL = '+input[i][j] +', '+input[i][j+1])
 else if(j===3)
 console.log('Hobby = ' +input[i][j+1])
-else{
-  console.log(' ')
+
 }
 }
+
 }
+
+console.log(datHAN( [
+                ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+                ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+                ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+                ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+            ]))
