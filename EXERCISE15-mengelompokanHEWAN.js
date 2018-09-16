d39ea5f76b63e9f7b471ca8c0bc524eac42d44b1
aandroomeedaa <41657603+aandroomeedaa@users.noreply.github.com>
2018-09-17 02:01:35 +0700

    Update EXERCISE15-mengelompokanHEWAN.js

--- EXERCISE15-mengelompokanHEWAN.js
+++ EXERCISE15-mengelompokanHEWAN.js
@@ -1,28 +1,27 @@
 function groupAnimals(animals) {
-  animals.sort();
-  var aR = [];
-  var fA = animals[0][0];
-  var aT = [];
-  
+  animals.sort()
+  var aR = []
+  var fA = animals[0][0]
+  var aT = []  
   for (var i = 0; i < animals.length; i++) {
     if (animals[i][0] === fA) {
-      aT.push(animals[i]);
+      aT.push(animals[i])
     } else {
-      aR.push(aT);
-      aT = [];
-      fA = animals[i][0];
-      aT.push(animals[i]);
+        aR.push(aT)
+        aT = []
+        fA = animals[i][0]
+        aT.push(animals[i])
+      }
+      if (i === animals.length-1) {
+        aR.push(aT);
+      }
     }
-    if (i === animals.length-1) {
-      aR.push(aT);
-    }
-  }
-  return aR;
+return aR;
 }
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda'] ]
-console.log('') //consolelog dibuat agar rapi
+
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
 // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
